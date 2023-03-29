76e840f4d03357976672b117f0421d11710c34b4
tomitaai <aiueoeuiam@gmail.com>
2023-03-29 14:46:28 +0000

    設定を更新

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,9 +1,23 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+    
+    	<!--ヘッダー[START]-->
+	<x-slot name="header">
+		<div class="flex justify-between">
+			<div class="flex">
+				<div class="sticky top-0 shrink-0">
+                    <a href="{{ route('dashboard') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    </a>
+                </div>
+        	</div>
+	        <div class="sm:flex sm:items-center">
+		        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+		            Profile
+		        </h2>
+            </div>
+        </div>
+	</x-slot>
+	<!--ヘッダー[END]-->
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -5,29 +5,15 @@
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            アカウントを削除すると、全てのデータが完全に削除されます。よろしければパスワードを入力してください。
         </p>
     </header>
 
-    <x-danger-button
-        x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
-
-    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
             @csrf
             @method('delete')
 
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('Are you sure you want to delete your account?') }}
-            </h2>
-
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
-            </p>
-
-            <div class="mt-6">
+            <div class="flex justify-center">
                 <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
 
                 <x-text-input
@@ -41,15 +27,11 @@
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
             </div>
 
-            <div class="mt-6 flex justify-end">
-                <x-secondary-button x-on:click="$dispatch('close')">
-                    {{ __('Cancel') }}
-                </x-secondary-button>
+            <div class="mt-6 flex justify-center">
 
                 <x-danger-button class="ml-3">
                     {{ __('Delete Account') }}
                 </x-danger-button>
             </div>
         </form>
-    </x-modal>
 </section>
